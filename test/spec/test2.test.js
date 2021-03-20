ab96ad539bc3131be77554471198eb91319e0635
wenfujie <1024402224@qq.com>
2021-03-20 09:34:04 +0800

    删除冗余文件

--- test/spec/test2.test.js
+++
@@ -1,6 +0,0 @@
-const Test = require('../need-test')
-const expect = require('chai').expect
-
-describe('两数相减', function () {
-  expect(Test.computed(2, 1)).to.be.equal(0)
-})
--- test/spec/testMocha.test.js
+++
@@ -1,8 +0,0 @@
-const Test = require('../need-test')
-const expect = require('chai').expect
-
-describe('加法测试', function () {
-  it('1+1应该等于2', function () {
-    expect(Test.add(1, 1)).to.be.equal(2)
-  })
-})
