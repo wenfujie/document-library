9a335f13156a1697f42ea29535e4f89e4386f3e8
温富杰 <wenfujie@dianchu.com>
2022-01-07 16:34:50 +0800

    更新部署脚本备注

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 ###
  # @Date: 2022-01-07 16:15:13
  # @LastEditors: wenfujie
- # @LastEditTime: 2022-01-07 16:21:12
+ # @LastEditTime: 2022-01-07 16:33:21
  # @FilePath: /document-library/deploy.sh
+ # @describe: 该脚本用于将代码同时推送到 github、gitee
+ # @use: 运行指令 sh deploy.sh
 ### 
 
 # 确保脚本抛出遇到的错误
@@ -11,7 +13,7 @@ set -e
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m '更新部署脚本备注'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
