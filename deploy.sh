efaaa523a61629de658276f668236e1bf25474c0
温富杰 <wenfujie@dianchu.com>
2022-01-12 14:00:11 +0800

    更新文章【articles/开发相关知识/学习正则表达式.md】

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2022-01-07 16:15:13
  # @LastEditors: wenfujie
- # @LastEditTime: 2022-01-12 11:35:58
+ # @LastEditTime: 2022-01-12 13:59:56
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 git init
 git add -A
-git commit -m '增加文章【articles/开发相关知识/学习正则表达式.md】'
+git commit -m '更新文章【articles/开发相关知识/学习正则表达式.md】'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
