0e23cf2e0317e50975591c21b4436713d686abee
温富杰 <wenfujie@dianchu.com>
2022-03-25 11:34:17 +0800

    增加文章【加密和签名】

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2022-01-07 16:15:13
  # @LastEditors: wenfujie
- # @LastEditTime: 2022-02-22 18:04:42
+ # @LastEditTime: 2022-03-25 11:34:09
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 git init
 git add -A
-git commit -m '更新【articles/Vue/Vue3快速上手.md】'
+git commit -m '增加文章【加密和签名】'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
