c8ec0a863d4b3a6584e7ff948d1a0d0e9b51b747
温富杰 <wenfujie@dianchu.com>
2022-10-03 10:22:28 +0800

    增加文章【articles/js相关/js小技巧.md】;调整readme目录结构

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 ###
  # @Date: 2022-01-07 16:15:13
- # @LastEditors: wenfujie
- # @LastEditTime: 2022-05-05 15:56:35
+ # @LastEditors: wfj
+ # @LastEditTime: 2022-10
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 git init
 git add -A
-git commit -m '增加文章【articles/开发相关知识/域名相关.md】'
+git commit -m '增加文章【articles/js相关/js小技巧.md】;调整readme目录结构'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
