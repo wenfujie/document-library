6b088bca8fc18b267271ad640bd6b35d5dd97007
温富杰 <wenfujie@dianchu.com>
2022-12-06 17:07:23 +0800

    测试gitee

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2022-01-07 16:15:13
  # @LastEditors: wfj
- # @LastEditTime: 2022-10
+ # @LastEditTime: 2022-12
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 git init
 git add -A
-git commit -m '增加文章【articles/js相关/js小技巧.md】;调整readme目录结构'
+git commit -m '测试gitee'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
