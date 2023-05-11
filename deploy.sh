16274c3e23b9e756a8a0101756cdc849633db5d7
温富杰 <wenfujie@dianchu.com>
2023-05-11 17:04:03 +0800

    发布文章【前端需储备开发语言】

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,8 @@ git init
 git add -A
 git commit -m '发布文章【前端需储备开发语言】'
 
-git push -f git@github.com:wenfujie/document-library.git master
+https://github.com/wenfujie/document-library.git
+git push -f git@github.com/wenfujie/document-library.git master
 # git push -f git@gitee.com:mozhata/document-library.git master
 
 echo '----- 恭喜，代码推送成功！ -----'
