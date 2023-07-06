b439ecf6af9d0d980f1d06eaf08d96d5f1245a8e
温富杰 <wenfujie@dianchu.com>
2023-07-06 22:47:21 +0800

    docs: 新增文章【一篇催眠自己去跑步的文章】

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2022-01-07 16:15:13
  # @LastEditors: wfj
- # @LastEditTime: 2023-06
+ # @LastEditTime: 2023-07
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 git init
 git add -A
-git commit -m 'docs: 更新[nginx指南]，增加内置变量小节'
+git commit -m 'docs: 新增文章【一篇催眠自己去跑步的文章】'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
