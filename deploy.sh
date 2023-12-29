6e01a64a8138c72ee8617ed6cdd09a360eb78fb7
温富杰 <wenfujie@dianchu.com>
2023-12-29 16:44:09 +0800

    docs: 编辑【vscode快捷键和终端指令】

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 ###
  # @Date: 2022-01-07 16:15:13
- # @LastEditors: wfj
- # @LastEditTime: 2023-12
+ # @LastEditors: 温富杰 wenfujie@dianchu.com
+ # @LastEditTime: 2023-12-29 16:44:05
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 # git init
 git add -A
-git commit -m 'docs: 编辑【编写声明文件】【常用配置】'
+git commit -m 'docs: 编辑【vscode快捷键和终端指令】'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
