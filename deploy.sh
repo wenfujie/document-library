953cfa920360b929c3d16105489fb2df4a2a9292
温富杰 <wenfujie@dianchu.com>
2024-01-25 11:45:37 +0800

    docs: 添加文章【零碎想法】

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2022-01-07 16:15:13
  # @LastEditors: 温富杰 wenfujie@dianchu.com
- # @LastEditTime: 2024-01-23 17:57:40
+ # @LastEditTime: 2024-01-25 11:45:22
  # @FilePath: /document-library/deploy.sh
  # @describe: 该脚本用于将代码同时推送到 github、gitee
  # @use: 运行指令 sh deploy.sh
@@ -13,7 +13,7 @@ set -e
 
 # git init
 git add -A
-git commit -m 'docs: 添加【手写代码】内容'
+git commit -m 'docs: 添加文章【零碎想法】'
 
 git push -f git@github.com:wenfujie/document-library.git master
 git push -f git@gitee.com:mozhata/document-library.git master
